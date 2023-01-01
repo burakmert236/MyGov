dee3a418d394d99cc981f2a267cd26c49772d633
Burak Mert <burakmert236@gmail.com>
2023-01-01 23:54:19 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import { useMetaMask } from "metamask-react";
+import { Button } from "antd";
+import contract from './useWeb3';
+
+import "./App.css";
+
+const ModelViewer = require('@metamask/logo');
 
 function App() {
+  const { status, connect, account, chainId, ethereum } = useMetaMask();
+
+  const [results, setResults] = useState({});
+
+  const logoRef = useRef();
+  const [refState, setRefState] = useState();
+
+  useEffect(() => {
+    if(refState) {
+      const viewer = ModelViewer({
+        // Dictates whether width & height are px or multiplied
+        pxNotRatio: true,
+        width: 300,
+        height: 300,
+        followMouse: true,
+      });
+    
+      const container = document.getElementById('logo-container');
+      console.log(container)
+      container.appendChild(viewer.container);
+    }
+  }, [refState])
+
+  if(status === "unavailable") {
+    return(<h2>Please use another browser with MetaMask extension</h2>)
+  }
+  
+  if (status === "notConnected" || status === "connecting") {
+    return(
+      <div className="connect-button-container">
+        <div id="logo-container" ref={el => {
+          logoRef.current = el;
+          if(!refState) setRefState(true);
+        }}></div>
+        <Button 
+          className="connect-button" 
+          type="primary" 
+          onClick={connect} 
+          loading={status === "connecting"} 
+          disabled={status === "connecting"}
+        >
+          {status === "connecting" ? "Connecting" : "Connect to MetaMask"}
+        </Button>
+      </div>
+    );
+  }
+
+  const handleFunctionCall = async (functionName, arguements) => {
+    if(functionName === "getNoOfSurveys") {
+      contract.methods.getNoOfSurveys().call().then(function(result) {
+        setResults(results => ({ ...results, noOfSurveys: result }));
+      });
+    }
+
+    if(functionName === "totalSupply") {
+      contract.methods.totalSupply().call().then(function(result) {
+        setResults(results => ({ ...results, totalSupply: result }));
+      });
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="function-container">
+        <Button onClick={() => handleFunctionCall("getNoOfSurveys")}>Get Number Of Surveys</Button>
+        {results?.noOfSurveys && <span>Number Of Surveys: <span className="bold">{results?.noOfSurveys}</span></span>}
+      </div>
+
+      <div className="function-container">
+        <Button onClick={() => handleFunctionCall("totalSupply")}>Get Total Supply</Button>
+        {results?.totalSupply && <span>Total Supply: <span className="bold">{results?.totalSupply}</span></span>}
+      </div>
     </div>
   );
 }
---
+++ src/contracts/index.js
@@ -0,0 +1,5 @@
+const my_gov_abi = require('./my_gov.json');
+
+module.exports = {
+    my_gov_abi
+}

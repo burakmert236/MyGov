f3b57dd97e02ff4ebbeff28d86c4ff959e7e6458
Burak Mert <burakmert236@gmail.com>
2023-01-03 14:15:44 +0300

    deploy init

--- src/useWeb3/index.js
+++ src/useWeb3/index.js
@@ -1,9 +1,9 @@
 import Web3 from 'web3';
 const { my_gov_abi } = require('../contracts');
 
-export const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
+export const web3 = new Web3(new Web3.providers.HttpProvider("https://core.bloxberg.org/RPC"));
 
-const contract_address = "0xc2663f661830dC52117379303780824a57A682A4";
+const contract_address = "0x9096fa8d183b4daa2e0c0532fe30d654ade386fc";
 export const contract = new web3.eth.Contract(my_gov_abi, contract_address);
 
 export default {contract, web3};
